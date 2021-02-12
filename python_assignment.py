ff76d51333266366e0fef09e050b397adaa3a7f6
Dipesh Bishwakarma <deepeesh471@gmail.com>
2021-02-12 07:26:29 +0545

    inital commit

---
+++ python_assignment.py
@@ -0,0 +1,174 @@
+# Data Types
+# 1. Write a Python program to count the number of characters (character
+# frequency) in a string. Sample String : google.com'
+# Expected Result : {'o': 3, 'g': 2, '.': 1, 'e': 1, 'l': 1, 'm': 1, 'c': 1}
+# 2. Write a Python program to get a string made of the first 2 and the last 2 chars
+# from a given a string. If the string length is less than 2, return instead of the
+# empty string.
+# Sample String : 'Python'
+# Expected Result : 'Pyon'
+# Sample String : 'Py'
+# Expected Result : 'PyPy'
+# Sample String : ' w'
+# Expected Result : Empty String
+# 3. Write a Python program to get a string from a given string where all
+# occurrences of its first char have been changed to '$', except the first char itself.
+# Sample String : 'restart'
+# Expected Result : 'resta$t'
+# 4. Write a Python program to get a single string from two given strings, separated
+# by a space and swap the first two characters of each string.
+# Sample String : 'abc', 'xyz'
+# Expected Result : 'xyc abz'
+# 5. Write a Python program to add 'ing' at the end of a given string (length should
+# be at least 3). If the given string already ends with 'ing' then add 'ly' instead. If the
+# string length of the given string is less than 3, leave it unchanged.
+# Sample String : 'abc'
+# Expected Result : 'abcing'
+# Sample String : 'string'
+# Expected Result : 'stringly'
+# 6. Write a Python program to find the first appearance of the substring 'not' and
+# 'poor' from a given string, if 'not' follows the 'poor', replace the whole 'not'...'poor'
+# substring with 'good'. Return the resulting string.
+# Sample String : 'The lyrics is not that poor!'
+# 'The lyrics is poor!'
+# Expected Result : 'The lyrics is good!'
+# 'The lyrics is poor!'
+# 7. Write a Python function that takes a list of words and returns the length of the
+# longest one.
+# 8. Write a Python program to remove the n
+# th
+# index character from a nonempty
+# string.
+# 9. Write a Python program to change a given string to a new string where the first
+# and last chars have been exchanged.
+# 10. Write a Python program to remove the characters which have odd index
+# values of a given string.
+# 11. Write a Python program to count the occurrences of each word in a given
+# sentence.
+# 12. Write a Python script that takes input from the user and displays that input
+# back in upper and lower cases.
+# 13. Write a Python program that accepts a comma separated sequence of words
+# as input and prints the unique words in sorted form (alphanumerically).
+# Sample Words : red, white, black, red, green, black
+# Expected Result : black, green, red, white,red
+# 14. Write a Python function to create the HTML string with tags around the
+# word(s).
+# Sample function and result :
+# add_tags('i', 'Python') -> '<i>Python</i>'
+# add_tags('b', 'Python Tutorial') -> '<b>Python Tutorial </b>'
+# 15. Write a Python function to insert a string in the middle of a string.
+# Sample function and result :
+# insert_sting_middle('[[]]<<>>', 'Python') -> [[Python]]
+# insert_sting_middle('{{}}', 'PHP') -> {{PHP}}
+# 16. Write a Python program to sum all the items in a list.
+# 17. Write a Python program to multiplies all the items in a list.
+# 18. Write a Python program to get the largest number from a list.
+# 19. Write a Python program to get the smallest number from a list.
+# 20. Write a Python program to count the number of strings where the string
+# length is 2 or more and the first and last character are same from a given list of
+# strings.
+# Sample List : ['abc', 'xyz', 'aba', '1221']
+# Expected Result : 2
+# 21. Write a Python program to get a list, sorted in increasing order by the last
+# element in each tuple from a given list of non-empty tuples.
+# Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+# Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+# 22. Write a Python program to remove duplicates from a list.
+# 23. Write a Python program to check a list is empty or not.
+# 24. Write a Python program to clone or copy a list.
+# 25. Write a Python program to check whether all dictionaries in a list are empty or
+# not.
+# Sample list : [{},{},{}]
+# Return value : True
+# Sample list : [{1,2},{},{}]
+# Return value : False
+# 26. Write a Python program to insert a given string at the beginning of all items in
+# a list.
+# Sample list : [1,2,3,4], string : emp
+# Expected output : ['emp1', 'emp2', 'emp3', 'emp4']
+# 27. Write a Python program to replace the last element in a list with another list.
+# Sample data : [1, 3, 5, 7, 9, 10], [2, 4, 6, 8]
+# Expected Output: [1, 3, 5, 7, 9, 2, 4, 6, 8]
+# 28. Write a Python script to add a key to a dictionary.
+# Sample Dictionary : {0: 10, 1: 20}
+# Expected Result : {0: 10, 1: 20, 2: 30}
+# 29. Write a Python script to concatenate following dictionaries to create a new
+# one.
+# Sample Dictionary :
+# dic1={1:10, 2:20}
+# dic2={3:30, 4:40}
+# dic3={5:50,6:60}
+# Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+# 30. Write a Python script to check whether a given key already exists in a
+# dictionary.
+# 31. Write a Python program to iterate over dictionaries using for loops.
+# 32. Write a Python script to generate and print a dictionary that contains a
+# number (between 1 and n) in the form (x, x*x).
+# Sample Dictionary ( n = 5) :
+# Expected Output : {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+# 33. Write a Python script to print a dictionary where the keys are numbers
+# between 1 and 15 (both included) and the values are square of keys
+# Sample Dictionary
+# {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144,
+# 13: 169, 14: 196, 15: 225}
+# 34. Write a Python script to merge two Python dictionaries.
+# 35. Write a Python program to iterate over dictionaries using for loops.
+# 36. Write a Python program to sum all the items in a dictionary.
+# 37. Write a Python program to multiply all the items in a dictionary.
+# 38. Write a Python program to remove a key from a dictionary.
+# 39. Write a Python program to unpack a tuple in several variables.
+# 40. Write a Python program to add an item in a tuple.
+# 41. Write a Python program to convert a tuple to a string.
+# 42. Write a Python program to convert a list to a tuple.
+# 43. Write a Python program to remove an item from a tuple.
+# 44. Write a Python program to slice a tuple.
+# 45. Write a Python program to find the index of an item of a tuple.
+# 46. Write a Python program to find the length of a tuple
+# Functions
+# 1. Write a Python function to find the Max of three numbers.
+# 2. Write a Python function to sum all the numbers in a list.
+# Sample List : (8, 2, 3, 0, 7)
+# Expected Output : 20
+# 3. Write a Python function to multiply all the numbers in a list.
+# Sample List : (8, 2, 3, -1, 7)
+# Expected Output : -336
+# 4. Write a Python program to reverse a string.
+# Sample String : "1234abcd"
+# Expected Output : "dcba4321"
+# 5. Write a Python function to calculate the factorial of a number (a non-negative
+# integer). The function accepts the number as an argument.
+# 6. Write a Python function to check whether a number is in a given range.
+# 7. Write a Python function that accepts a string and calculate the number of
+# upper case letters and lower case letters.
+# Sample String : 'The quick Brow Fox'
+# Expected Output :
+# No. of Upper case characters : 3
+# No. of Lower case Characters : 12
+# 8. Write a Python function that takes a list and returns a new list with unique
+# elements of the first list.
+# Sample List : [1,2,3,3,3,3,4,5]
+# Unique List : [1, 2, 3, 4, 5]
+# 9. Write a Python function that takes a number as a parameter and check the
+# number is prime or not.
+# Note : A prime number (or a prime) is a natural number greater than 1 and that
+# has no positive divisors other than 1 and itself.
+# 10. Write a Python program to print the even numbers from a given list.
+# Sample List : [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Expected Result : [2, 4, 6, 8]
+# 11. Write a Python program to create a lambda function that adds 15 to a given
+# number passed in as an argument, also create a lambda function that multiplies
+# argument x with argument y and print the result.
+# 12. Write a Python program to create a function that takes one argument, and
+# that argument will be multiplied with an unknown given number.
+# 13. Write a Python program to sort a list of tuples using Lambda.
+# 14. Write a Python program to sort a list of dictionaries using Lambda.
+# 15. Write a Python program to filter a list of integers using Lambda.
+# 16. Write a Python program to square and cube every number in a given list of
+# integers using Lambda.
+# 17. Write a Python program to find if a given string starts with a given character
+# using Lambda.
+# 18. Write a Python program to check whether a given string is number or not
+# using Lambda.
+# 19. Write a Python program to create Fibonacci series upto n using Lambda.
+# 20. Write a Python program to find intersection of two given arrays using
+# Lambda.
